d9bd38d62d6b48eebbd55556f95431c5a41bbecb
Rodrick__ <tom.rodrick100@gmail.com>
2026-08-25 19:14:26 +0200

    Dobře tak ještě jinak..

--- src/example-config.py
+++
@@ -1,4 +0,0 @@
-token = None # tohleto tady nech takhle
-usrname = "" # změňte za uživ. jméno co máte v Bakalářích
-pswrd = "" # změňte za heslo co máte v Bakalářích
-urlskoly = "" # změňte za url školy co máte v Bakalářích
